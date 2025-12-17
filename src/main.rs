a16c6b6e0ea2e54a7f93581aa946849b48b7e5af
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-12-17 12:51:33 -0600

    advanced_traits

--- src/main.rs
+++ src/main.rs
@@ -96,13 +96,207 @@ fn main() {
     }
     // impl unsafe trait
     {
-        unsafe trait Foo {
+        unsafe trait _Foo {
             // methods go here
         }
-        unsafe impl Foo for i32 {
+        unsafe impl _Foo for i32 {
             // method implementations go here
         }
     }
-    // advanced traits
-    
+    // Advanced traits
+    // defining traits with associated types
+    {
+        trait _Iterator {
+            type Item;
+
+            fn next(&mut self) -> Option<Self::Item>;
+        }
+    }
+    {
+        #[derive(Debug)]
+        struct Counter {
+            count: u32,
+        }
+
+        impl Counter {
+            fn new() -> Counter {
+                Counter { count: 0 }
+            }
+        }
+
+        // pub trait Iterator<T> {
+        //     fn next(&mut self) -> Option<T>;
+        // }
+
+        impl Iterator for Counter {
+            type Item = u32;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                // --snip--
+                if self.count < 5 {
+                    self.count += 1;
+                    Some(self.count)
+                } else {
+                    None
+                }
+            }
+        }
+        println!("Counter created: {:?}", Counter::new());
+    }
+    // using default generic parameters and operator overloading
+    {
+        use std::ops::Add;
+
+        #[derive(Debug, Copy, Clone, PartialEq)]
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        impl Add for Point {
+            type Output = Point;
+
+            fn add(self, other: Point) -> Point {
+                Point { 
+                    x: self.x + other.x, 
+                    y: self.y + other.y 
+                }
+            }
+        }
+        
+        assert_eq!(
+            Point {x: 1, y: 0} + Point {x: 2, y: 3},
+            Point {x: 3, y: 3}
+        );
+        println!("Point addition: {:?}", Point {x: 1, y: 0} + Point {x: 2, y: 3});
+        
+    }
+    {
+        #![allow(unused)]
+        trait Add<Rhs=Self> {
+            type Output;
+            fn add(self, rhs: Rhs) -> Self::Output;
+        }   
+    }
+    {
+        use std::ops::Add;
+
+        #[derive(Debug)]
+        struct Millimeters(u32);
+        struct Meters(u32);
+
+        impl Add<Meters> for Millimeters {
+            type Output = Millimeters;
+
+            fn add(self, other: Meters) -> Millimeters {
+                Millimeters(self.0 + (other.0 * 1000))
+            }
+        }
+        println!("Millimeters + Meters = {:?}", Millimeters(500) + Meters(2));
+    }
+    // Disambiguating between identically named methods
+    {
+        trait Pilot {
+            fn fly(&self);
+        }
+
+        trait Wizard {
+            fn fly(&self);
+        }
+
+        struct Human;
+
+        impl Pilot for Human {
+            fn fly(&self) {
+                println!("This is your captain speaking.");
+            }
+        }
+
+        impl Wizard for Human {
+            fn fly(&self) {
+                println!("Up!");
+            }
+        }
+
+        impl Human {
+            fn fly(&self) {
+                println!("*waving arms furiously*");
+            }
+        }
+
+        let person = Human;
+        person.fly();
+        Pilot::fly(&person);
+        Wizard::fly(&person);
+    }
+    {
+        trait Animal {
+            fn baby_name() -> String;
+        }
+
+        struct Dog;
+
+        impl Dog {
+            fn _baby_name() -> String {
+                String::from("Spot")
+            }
+        }
+
+        impl Animal for Dog {
+            fn baby_name() -> String {
+                String::from("puppy")
+            }
+        }
+
+        println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+    }
+    // using supertraits
+    {
+        
+        trait OutlinePrint: fmt::Display {
+            fn outline_print(&self) {
+                let output = self.to_string();
+                let len = output.len();
+                println!("{}", "*".repeat(len + 4));
+                println!("*{}*", " ".repeat(len + 2));
+                println!("* {} *", output);
+                println!("*{}*", " ".repeat(len + 2));
+                println!("{}", "*".repeat(len + 4));
+            }
+        }
+        
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+        
+        impl OutlinePrint for Point {}
+        
+        use std::fmt;
+
+        impl fmt::Display for Point {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "({}, {})", self.x, self.y)
+            }
+        }
+
+        let p = Point {x: 1, y: 3};
+        p.outline_print();
+    }
+    // implementing external traits with the newtype patern
+    {
+        use std::fmt;
+
+        struct Wrapper(Vec<String>);
+
+        impl fmt::Display for Wrapper {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "[{}]", self.0.join(", "))
+            }
+        }
+
+        let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+        println!("w = {w}");
+    }
+    {}
 }
